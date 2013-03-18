61cbd54f01e8fe885f8148a568bf7c3c0924df2c
brajes <brajes@wyrm.(none)>
2013-03-19 03:54:57 +0700

    Initial Commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Napakrimba::Application.config.secret_token = '289af1c74d607ce3ada5567655fc63611c9be056b3afd4801c17424d8ed49b64d8958750d3ee29b8e2a4fd177bd0d998259563bed99d87bf4a3796cb28f56818'
